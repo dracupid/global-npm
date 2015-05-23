5a59d14d86cb0f9f0dd62fb8ad9ddffb4792a727
Zhao Jingchen <dracupid@gmail.com>
2015-05-23 08:50:59 +0800

    handler which error

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 "use strict";
-
 var fs = require('fs'),
     path = require('path'),
     which = require('which');
 
-var GLOBAL_NPM_BIN = fs.realpathSync(which.sync('npm')),
-    GLOBAL_NPM_PATH = path.join(GLOBAL_NPM_BIN,
-        process.platform === "win32" ? '../node_modules/npm' : '../..');
+var GLOBAL_NPM_BIN, GLOBAL_NPM_PATH;
 
-module.exports = (function(npmPath) {
+function throwNotFoundError() {
+    var err = new Error("Cannot find module 'npm'");
+    err.code = 'MODULE_NOT_FOUND';
+    throw err;
+}
+
+try {
+    GLOBAL_NPM_BIN = fs.realpathSync(which.sync('npm'));
+}
+catch (e) {
+    throwNotFoundError();
+}
+
+GLOBAL_NPM_PATH = path.join(GLOBAL_NPM_BIN, process.platform === "win32" ? '../node_modules/npm' : '../..');
+
+module.exports = (function (npmPath) {
     try {
         var npm = require(npmPath);
         return npm;
-    } catch (e) {
-        if(e.code !== 'MODULE_NOT_FOUND'){
+    }
+    catch (e) {
+        if (e.code !== 'MODULE_NOT_FOUND') {
             throw e;
         }
-        var err = new Error("Cannot find module 'npm'");
-        err.code = 'MODULE_NOT_FOUND';
-        throw err;
+        throwNotFoundError();
     }
 })(GLOBAL_NPM_PATH);
-
 module.exports.GLOBAL_NPM_PATH = GLOBAL_NPM_PATH;
 module.exports.GLOBAL_NPM_BIN = GLOBAL_NPM_BIN;
